f4e9040623c3fe78654d2c9a39c6442f3da01dd7
nikolay35977 <nikolay.sychev.1999@gmail.com>
2021-07-26 19:41:01 +0300

    add skill page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./Components/HomePage";
 import "./Assets/styles/scss/main.scss";
 import ServicesPage from "./Components/ServicesPage";
 import WorkPage from "./Components/WorkPage";
+import SkillPage from "./Components/SkillPage";
 
 const App = (props) => {
   return (
@@ -10,6 +11,7 @@ const App = (props) => {
         <HomePage/>
         <ServicesPage/>
         <WorkPage/>
+        <SkillPage/>
     </div>
   );
 }
---
+++ src/Components/SkillPage.js
@@ -0,0 +1,37 @@
+﻿import React from 'react';
+
+const SkillCard = (props) => {
+    return (
+        <div className={'skill_card'}>
+            <div className={'percent'}>
+                21
+            </div>
+            <div className={'text'}>
+                PROGRAMMING
+            </div>
+        </div>
+    )
+}
+
+const SkillPage = (props) => {
+    return (
+        <section id='skill'>
+            <div className={'container'}>
+                <div className={'skill_header'}>
+                    <div className='title'>
+                        my skills
+                    </div>
+                    <div className='subtitle'>
+                        No one lights a lamp in order to hide it behind the door: the purpose of light is to create
+                        more light, to open people’s eyes, to reveal the marvels around.
+                    </div>
+                </div>
+                <div className={'skill_cards'}>
+                    <SkillCard/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SkillPage;
